7df05b4e41dbc21107c261d6a59cc982e12fbfae
agatan <agatan039@gmail.com>
2016-08-08 17:35:05 +0900

    Remove unused functions

--- crepe.cpp
+++ crepe.cpp
@@ -8,24 +8,6 @@
 #include "walker.hpp"
 #include "option.hpp"
 
-struct match {
-  std::string contents;
-  int linum;
-};
-
-std::ostream& operator<<(std::ostream& os, match const& m) {
-  return os << m.linum << ": " << m.contents;
-}
-
-bool make_match(int linum, std::string const& line, std::string const& needle, match& m) {
-  if (line.find(needle) == std::string::npos) {
-    return false;
-  }
-  m.contents = line;
-  m.linum = linum;
-  return true;
-}
-
 int main(int argc, char **argv) {
   crepe::init_option(argc, argv);
 
